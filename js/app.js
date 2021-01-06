888184f5517e93513545ba04c078b78440ef6ec5
nisreenissa <nisreentayseer93@hotmail.com>
2021-01-06 14:12:41 +0200

    msg

---
+++ js/app.js
@@ -0,0 +1,66 @@
+'use strict'
+
+var username=prompt('please enter you name');
+alert('Wlecome ! and Thank you for visting us '+username );
+
+
+var Q1 =prompt('do you have computer skills ? answer with yes or no ');
+
+if (Q1.toLowerCase==='yes'){
+    //console.log('if you have computer skills then you can be my partner');
+
+alert('fantastic!')
+}
+
+else{
+    alert('You must have cmputer skills');
+}
+
+
+var Q2 =prompt('do you like HTML ? answer with yes or no ');
+
+if (Q2.toLowerCase==='yes'){
+    //console.log('if you have computer skills then you can be my partner');
+
+alert('fantastic!');}
+
+else{
+    alert('You must learn HTML  to join us');
+}
+
+
+
+var Q3 =prompt('do you like CSS ? answer with yes or no ');
+
+if (Q3.toLowerCase==='yes'){
+    //console.log(' you can be my partner');
+
+alert('fantastic!');}
+
+else{
+    alert('You must learn CSS  to join us');
+}
+
+var Q4 =prompt('do you like JavaScript ? answer with yes or no');
+
+if (Q4.toLowerCase==='yes'){
+    //console.log(' you can be my partner');
+
+alert('fantastic!');}
+
+else{
+    alert('You must learn JavaScript  to join us');
+}
+
+var Q5 =prompt('do you like ltuc ? answer with yes or no , Or you can answer with y or n');
+
+
+if (Q5.toLowerCase==='yes'){
+    //console.log(' you can be my partner');
+
+alert('fantastic!');}
+
+else{
+    alert('You can read about us and join us later');
+}
+
