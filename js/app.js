9196dd3c3d83b8df9ebffd24e99a9e2d6ca3982a
nisreenissa <nisreentayseer93@hotmail.com>
2021-01-08 17:53:51 +0200

    m

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 // var username = prompt('please enter you name');
-// alert('Wlecome ! and Thank you for visting us ' + username);
+// alert('Welcome  ! and Thank you for visting us ' + username);
 
 // var Q1 = prompt('do you have computer skills ? answer with yes or no or with y or n');
 
 // if (Q1.toLowerCase() === 'yes' || Q1.toLowerCase() === 'y') {
 // alert('fantastic!');
+//var score=score+1;
 //     //console.log('if you have computer skills then you can be my partner');
 // }
 
@@ -24,6 +25,7 @@
 // if (Q2.toLowerCase() === 'yes' || Q2.toLowerCase() === 'y') {
 //     //console.log('if you have computer skills then you can be my partner');
 //     alert('fantastic!');
+//score=score++;
 // } else if (Q2.toLowerCase() === 'no' || Q2.toLowerCase() === 'n') {
 //     alert('too bad you should practice');
 // }
@@ -39,6 +41,7 @@
 // if (Q3.toLowerCase() === 'yes' || Q3.toLowerCase() === 'y') {
 //     //console.log(' you can be my partner');
 //     alert('fantastic!');
+//score=score++;
 // }
 
 // else if (Q3.toLowerCase() === 'no' || Q3.toLowerCase() === 'n') {
@@ -81,156 +84,339 @@
 // // }
 
 //lab3
-Var score =0;
-var myAge = 27;
-var attempts  = 0;
-while (attempts < 4){
-attempts++;
- var UserInput = parseInt(prompt('Quess  my age?'));
+// Var score =0;
+// var myAge = 27;
+// var attempts  = 0;
+// while (attempts < 4){
+// attempts++;
+//  var UserInput = parseInt(prompt('Quess  my age?'));
 
-if (UserInput === myAge) {
-alert('Yes that is correct');
-score = score + 1 ;
+// if (UserInput === myAge) {
+// alert('Yes that is correct');
+// score = score + 1 ;
 
- break;
-} 
-else if (UserInput > myAge) {
-alert ('Too high!')
-} 
-else if (UserInput < myAge) {
- alert('Too low!');
-}  
+//  break;
+// } 
+// else if (UserInput > myAge) {
+// alert ('Too high!')
+// } 
+// else if (UserInput < myAge) {
+//  alert('Too low!');
+// }  
 
-}
-
-
-var colledgeYears = 4 ;
-var attempts  = 0;
-while (attempts < 4) {
-attempts++;
- var UserInput = parseInt(prompt('Quess  how many years i spent in the universsity?'));
-
-if (UserInput === colledgeYears) {
-alert('Yes that is correct');
-score = score + 1 ;
-
- break;
-} 
-else if (UserInput >colledgeYears) {
-alert ('Too high!')
-} 
-else if (UserInput < colledgeYears) {
- alert('Too low!');
-}
-}
+// }
 
 
-var Sisters = 4 ;
-var attempts  = 0;
-while (attempts < 4) {
-attempts++;
- var UserInput = parseInt(prompt('Quess  how many sisters i have?'));
+// var colledgeYears = 4 ;
+// var attempts  = 0;
+// while (attempts < 4) {
+// attempts++;
+//  var UserInput = parseInt(prompt('Quess  how many years i spent in the universsity?'));
 
-if (UserInput === Sisters) {
-alert('Yes that is correct');
-score = score + 1 ;
+// if (UserInput === colledgeYears) {
+// alert('Yes that is correct');
+// score = score + 1 ;
 
- break;
-} 
-else if (UserInput >Sisters) {
-alert ('Too high!')
-} 
-else if (UserInput < Sisters) {
- alert('Too low!');
-}
-}
-
-var brothers = 3 ;
-var attempts  = 0;
-while (attempts < 4) {
-attempts++;
- var UserInput = parseInt(prompt('Quess  how many sisters i have?'));
-
-if (UserInput === brothers) {
-alert('Yes that is correct');
-score = score + 1 ;
-
- break;
-} 
-else if (UserInput > brothers) {
-alert ('Too high!')
-} 
-else if (UserInput < brothers) {
- alert('Too low!');
-}
-}
-
-var travels = 6 ;
-var attempts  = 0;
-while (attempts < 4) {
-attempts++;
- var UserInput = parseInt(prompt('Quess  how many many time i have traveled ?'));
-
-if (UserInput === travels) {
-alert('Yes that is correct');
-score = score + 1 ;
-
- break;
-} 
-else if (UserInput > travels) {
-alert ('Too high!')
-} 
-else if (UserInput < travels) {
- alert('Too low!');
-}
-}
-
-//Q1 
-
-// var q7 = ['nada','rawan','yasmin','raya'];
-// var guess;
-// var flag = false;
-// var counter = 0;
-// for(var i = 0 ; i < 6; i++){
-//    guess = prompt('whos my fav friend');
-//    for(var j = 0; q7.length; j++){
-//        if(guess === q7[j]){
-//            alert('correct answer');
-//            flag = true;
-//            break;
-//        }else{
-//            guess = prompt('false try again');
-//            counter++;
-//            if(counter === 6){
-//                alert('herea are my fav friends '+q7)
-//                break;
-//            }
-//        }
-//    }
-//    if(flag === true){
-//        break;
-//    }
-
-    // i will ask the user each time and i will loop through the array
-    
+//  break;
+// } 
+// else if (UserInput >colledgeYears) {
+// alert ('Too high!')
+// } 
+// else if (UserInput < colledgeYears) {
+//  alert('Too low!');
+// }
+// }
 
 
-var Q7 =['nada','rawan','yasmin','raya'];
-for (var c = 6 ; c >= 1 ; c--){
-    for(var Index=0 ; Index <=3 ;Index++){
-        var UserQ7=prompt('guess one of my favourite firends');
+// var Sisters = 4 ;
+// var attempts  = 0;
+// while (attempts < 4) {
+// attempts++;
+//  var UserInput = parseInt(prompt('Quess  how many sisters i have?'));
+
+// if (UserInput === Sisters) {
+// alert('Yes that is correct');
+// score = score + 1 ;
+
+//  break;
+// } 
+// else if (UserInput >Sisters) {
+// alert ('Too high!')
+// } 
+// else if (UserInput < Sisters) {
+//  alert('Too low!');
+// }
+// }
+
+// var brothers = 3 ;
+// var attempts  = 0;
+// while (attempts < 4) {
+// attempts++;
+//  var UserInput = parseInt(prompt('Quess  how many sisters i have?'));
+
+// if (UserInput === brothers) {
+// alert('Yes that is correct');
+// score = score + 1 ;
+
+//  break;
+// } 
+// else if (UserInput > brothers) {
+// alert ('Too high!')
+// } 
+// else if (UserInput < brothers) {
+//  alert('Too low!');
+// }
+// }
+
+// var travels = 6 ;
+// var attempts  = 0;
+// while (attempts < 4) {
+// attempts++;
+//  var UserInput = parseInt(prompt('Quess  how many many time i have traveled ?'));
+
+// if (UserInput === travels) {
+// alert('Yes that is correct');
+// score = score + 1 ;
+
+//  break;
+// } 
+// else if (UserInput > travels) {
+// alert ('Too high!')
+// } 
+// else if (UserInput < travels) {
+//  alert('Too low!');
+// }
+// }
+
+// var colors = 6 ;
+// var attempts  = 0;
+// while (attempts < 4) {
+// attempts++;
+//  var UserInput = parseInt(prompt('Quess  how many colors i like ?'));
+
+// if (UserInput === colors) {
+// alert('Yes that is correct');
+// score = score + 1 ;
+
+//  break;
+// } 
+// else if (UserInput > colors) {
+// alert ('Too high!')
+// } 
+// else if (UserInput < colors) {
+//  alert('Too low!');
+// }
+// }
 
 
-    if(UserQ7.toLowerCase()===Q7[Index]){
-    alert('this is a correct answer');
-    score = score + 1 ;
-    
+
+
+
+// var Q7 = ['nada', 'rawan', 'yasmin', 'raya'];
+// var UserQ7 = prompt('guess one of my favourite firends');
+
+
+// for (var c = 0; c < 5; c++) {
+//     for (var Index = 0; Index < Q7.length; Index++) {
+
+
+//         if (UserQ7.toLowerCase() === Q7[Index]) {
+//             alert('this is a correct answer');
+//             //     score = score + 1 ;
+//             c = 5;
+//             break;
+//         }
+
+//     }
+//      if(c==5){
+//          break;
+//      }
+//      UserQ7 = prompt('try again');
+
+// }
+
+//     alert('your score is'+score);
+
+// lab4
+
+function question1() {
+    Var score = 0;
+    var myAge = 27;
+    var attempts = 0;
+    while (attempts < 4) {
+        attempts++;
+        var UserInput = parseInt(prompt('Quess  my age?'));
+
+        if (UserInput === myAge) {
+            alert('Yes that is correct');
+            score = score + 1;
+
+            break;
+        }
+        else if (UserInput > myAge) {
+            alert('Too high!')
+        }
+        else if (UserInput < myAge) {
+            alert('Too low!');
+        }
     }
-    else{ alert('that is incorrect');
-}
-    }}
+    question1();
 
-    alert('your score is'+score);
+    function question2() {
 
+        var colledgeYears = 4;
+        var attempts = 0;
+        while (attempts < 4) {
+            attempts++;
+            var UserInput = parseInt(prompt('Quess  how many years i spent in the universsity?'));
+
+            if (UserInput === colledgeYears) {
+                alert('Yes that is correct');
+                score = score + 1;
+
+                break;
+            }
+            else if (UserInput > colledgeYears) {
+                alert('Too high!')
+            }
+            else if (UserInput < colledgeYears) {
+                alert('Too low!');
+            }
+        }
+
+
+    }
+    question2();
+
+    function question3() {
+
+        var Sisters = 4;
+        var attempts = 0;
+        while (attempts < 4) {
+            attempts++;
+            var UserInput = parseInt(prompt('Quess  how many sisters i have?'));
+
+            if (UserInput === Sisters) {
+                alert('Yes that is correct');
+                score = score + 1;
+
+                break;
+            }
+            else if (UserInput > Sisters) {
+                alert('Too high!')
+            }
+            else if (UserInput < Sisters) {
+                alert('Too low!');
+            }
+        }
+
+    }
+
+    question3();
+
+    function question4() {
+
+        var brothers = 3;
+        var attempts = 0;
+        while (attempts < 4) {
+            attempts++;
+            var UserInput = parseInt(prompt('Quess  how many sisters i have?'));
+
+            if (UserInput === brothers) {
+                alert('Yes that is correct');
+                score = score + 1;
+
+                break;
+            }
+            else if (UserInput > brothers) {
+                alert('Too high!')
+            }
+            else if (UserInput < brothers) {
+                alert('Too low!');
+            }
+        }
+
+    }
+
+    question4();
+
+    function question5(){
+        var travels = 6;
+        var attempts = 0;
+        while (attempts < 4) {
+            attempts++;
+            var UserInput = parseInt(prompt('Quess  how many many time i have traveled ?'));
+
+            if (UserInput === travels) {
+                alert('Yes that is correct');
+                score = score + 1;
+
+                break;
+            }
+            else if (UserInput > travels) {
+                alert('Too high!')
+            }
+            else if (UserInput < travels) {
+                alert('Too low!');
+            }
+        }
+    }
+
+    question5();
+
+    
+            
+        function question6() {
+            var colors = 6;
+            var attempts = 0;
+            while (attempts < 4) {
+                attempts++;
+                var UserInput = parseInt(prompt('Quess  how many colors i like ?'));
+
+                if (UserInput === colors) {
+                    alert('Yes that is correct');
+                    score = score + 1;
+
+                    break;
+                }
+                else if (UserInput > colors) {
+                    alert('Too high!')
+                }
+                else if (UserInput < colors) {
+                    alert('Too low!');
+                }
+            }
+        }
+        question6();
+
+    
+    
+function question7{
+        var Q7 = ['nada', 'rawan', 'yasmin', 'raya'];
+        var UserQ7 = prompt('guess one of my favourite firends');
+        
+        
+        for (var c = 0; c < 5; c++) {
+            for (var Index = 0; Index < Q7.length; Index++) {
+        
+        
+                if (UserQ7.toLowerCase() === Q7[Index]) {
+                    alert('this is a correct answer');
+                    //     score = score + 1 ;
+                    c = 5;
+                    break;
+                }
+        
+            }
+             if(c==5){
+                 break;
+             }
+             UserQ7 = prompt('try again');
+        
+        }
+        
+            alert('your score is'+score);
+    }
+        question7();
 
 
